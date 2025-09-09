478fc765d3ee2e8f98746fcfbc3e82a01b9639d7
karin27-06 <karinsntmr@gmail.com>
2025-09-09 15:54:10 -0500

    dashboard vacio

--- resources/js/layout/AppFooter.vue
+++ resources/js/layout/AppFooter.vue
@@ -3,13 +3,6 @@
 <template>
   <div class="layout-footer w-full bg-surface-100 dark:bg-surface-900 py-4 px-4 sm:px-6 md:px-8 text-center text-sm sm:text-base flex flex-col sm:flex-row items-center justify-center gap-1 sm:gap-2">
     <span>Copyright © Sistema de reconocimiento 2025 -</span>
-    <a
-      href="https://www.tjingenieria.com/"
-      target="_blank"
-      rel="noopener noreferrer"
-      class="text-primary font-bold hover:underline break-words"
-    >
-      Soluciones en Ingenieria T&J EIRL.
-    </a>
+      P.L y K.C.
   </div>
 </template>
