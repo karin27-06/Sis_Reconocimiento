ad4bef674ab6880527e0897c0cdbcadd4e4eb375
karin27-06 <karinsntmr@gmail.com>
2025-09-10 14:48:09 -0500

    TS-4

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -27,6 +27,7 @@ class UserFactory extends Factory
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'username' => fake()->unique()->userName(),
+            'dni' => $this->faker->unique()->numerify('########'),
             'status' => 1,
             'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
