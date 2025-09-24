8bee28b389577cbaca2b39a0243db3274660b095
Pablo Lupu <pablolupu2020@gmail.com>
2025-09-24 12:31:40 -0500

    modificaciones api

--- resources/js/pages/panel/Movement/Desarrollo/ListMovimiento.vue
+++ resources/js/pages/panel/Movement/Desarrollo/ListMovimiento.vue
@@ -148,10 +148,6 @@ const getSeverity = (value: boolean) => (value ? 'success' : 'danger');
 onMounted(() => {
     loadMovimientos();
 });
-const tipoTemplate = (rowData) => {
-  return rowData.idTipo === 1 ? 'Cara' : rowData.idTipo === 2 ? 'Huella' : 'Desconocido';
-};
-
 </script>
 
 <template>
@@ -209,13 +205,7 @@ const tipoTemplate = (rowData) => {
 
     <Column selectionMode="multiple" style="width: 1rem" />
     <Column field="Espacio" header="Espacio" sortable style="min-width: 12rem" />
-<Column 
-  field="idTipo" 
-  header="Tipo" 
-  sortable 
-  style="min-width: 10rem"
-  :body="tipoTemplate" 
-/>
+    <Column field="idTipo" header="Tipo" sortable style="min-width: 10rem" />
     <Column field="reconocido" header="Reconocido" sortable style="min-width: 8rem">
         <template #body="{ data }">
             <Tag :value="data.reconocido ? 'Sí' : 'No'" :severity="getSeverity(data.reconocido)" />
